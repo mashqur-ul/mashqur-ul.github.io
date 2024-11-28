53f36053d543bb7944082f1752366ef1e950c8bc
Zafree <zafree@Zafrees-MacBook-Pro.local>
2024-11-28 13:54:12 +0600

    sass @import and percentage deprecated resolve

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          api: 'modern-compiler', // or 'modern'
+          silenceDeprecations: ['import']
+        },
+      },
+    },
+  }
+});
